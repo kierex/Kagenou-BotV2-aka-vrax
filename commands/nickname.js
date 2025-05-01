113c50cd43c78e5c7ee66faeaacf23a87f7793fc
kierex <heroxhonattackz20@gmail.com>
2025-05-02 07:21:03 +0800

    Update nickname.js

--- commands/nickname.js
+++ commands/nickname.js
@@ -1,45 +1,32 @@
 const config = require("../config.json");
 
 module.exports = {
-
     name: "nickname",
-
     handleEvent: true,
 
     async handleEvent({ api, event }) {
-
-        if (
-
-            event.logMessageType === "log:subscribe" &&
-
-            event.logMessageData.addedParticipants.some(user => user.userFbId === api.getCurrentUserID())
-
-        ) {
-
-            try {
-
+        try {
+            // Check if the event is a subscription (user added to the thread)
+            if (
+                event.logMessageType === "log:subscribe" &&
+                event.logMessageData.addedParticipants.some(user => user.userFbId === api.getCurrentUserID())
+            ) {
+                // Get the bot's ID and name
                 const botID = api.getCurrentUserID();
-
                 const botName = config.botName || "Bot";
 
-                
-
-          
-
+                // Change the nickname of the bot in the thread
                 api.changeNickname(botName, event.threadID, botID, (err) => {
-
-                    if (err) console.error("Failed to set nickname:", err);
-
+                    if (err) {
+                        console.error("Failed to set nickname:", err);
+                    } else {
+                        console.log(`Successfully set bot nickname to "${botName}" in thread ${event.threadID}`);
+                    }
                 });
-
-            } catch (error) {
-
-                console.error("Error in nickname event:", error);
-
             }
-
+        } catch (error) {
+            // Log any unexpected errors
+            console.error("Error in nickname event handler:", error);
         }
-
     }
-
 };
