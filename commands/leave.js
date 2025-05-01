040902b74f6d603f3e559b836fafc163c2c94f75
kierex <heroxhonattackz20@gmail.com>
2025-05-01 09:25:39 +0800

    Update leave.js

--- commands/leave.js
+++ commands/leave.js
@@ -15,7 +15,7 @@ module.exports = {
                 const userInfo = await api.getUserInfo(leftUserID);
                 const userName = userInfo[leftUserID]?.name || "Someone";
 
-                api.sendMessage(`😢 ${userName} has left the group. We'll miss you!`, threadID);
+                api.sendMessage(`😢 ${userName} has left the group. haha buti naman backstab niyo nga ang asim naman ew`, threadID);
             } catch (error) {
                 console.error("Error fetching user info:", error);
             }
