0e2000145a9c2e09eadb0888f7bed6f21d09da70
kierex <heroxhonattackz20@gmail.com>
2025-05-06 18:00:13 +0800

    Update restart.js

--- commands/restart.js
+++ commands/restart.js
@@ -3,13 +3,24 @@ module.exports = {
     description: 'Restarts the bot.',
     execute: async (api, event, args, commands, prefix, admins, appState, sendMessage) => {
         const { threadID, senderID } = event;
+
+        // Check if the sender is an admin
         if (!admins.includes(senderID)) {
-            sendMessage(api, { threadID, message: 'You do not have permission to use this command.' });
-            return;
+            return sendMessage(api, {
+                threadID,
+                message: '❌ You do not have permission to use this command.',
+            });
         }
-        sendMessage(api, { threadID, message: 'Restarting...' });
+
+        // Notify restart
+        await sendMessage(api, {
+            threadID,
+            message: '♻ Restarting the bot...',
+        });
+
+        // Exit process after short delay
         setTimeout(() => {
             process.exit(0);
-        }, 1000); 
+        }, 1000);
     }
 };
